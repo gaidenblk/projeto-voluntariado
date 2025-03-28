dce23d859e5ec1905df7d14ace265288881e34ed
Leandro Santos <gaiden_leo@hotmail.com>
2025-03-28 11:51:09 -0300

    Adicionado sistema de Migration

---
+++ api/src/database/migrate.js
@@ -0,0 +1,41 @@
+import pg from "pg";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import config from "../config/index.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const client = new pg.Client({
+	connectionString: config.DATABASE_URL,
+});
+
+async function runMigrations() {
+	try {
+		await client.connect();
+		const files = fs
+			.readdirSync(__dirname)
+			.filter((file) => file.endsWith(".sql"))
+			.sort();
+
+		console.log("Executing Migrations...");
+		await client.query("BEGIN");
+
+		for (const file of files) {
+			const filePath = path.join(__dirname, file);
+			const sql = fs.readFileSync(filePath, "utf-8");
+			await client.query(sql);
+			console.log(`Migration ${file} executed successfully.`);
+		}
+
+		await client.query("COMMIT");
+	} catch (error) {
+		await client.query("ROLLBACK");
+		console.error("Error running migrations:", error);
+	} finally {
+		console.log("Finished running Migrations!");
+		await client.end();
+	}
+}
+
+runMigrations();
